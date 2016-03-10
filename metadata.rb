418d0d25daedb13558cedfbbbc9425a57e06d070
Alexey Noskov <alexey.noskov@gmail.com>
2016-03-10 22:08:32 +0300

    Add source/issues urls to metadata

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,8 @@ license          "MIT"
 description      "Installs Mjpg streamer."
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
 version          "0.1.0"
+source_url      'https://github.com/alno/chef-mjpg-streamer' if respond_to?(:source_url)
+issues_url      'https://github.com/alno/chef-mjpg-streamer/issues' if respond_to?(:issues_url)
 
 recipe "mjpg-streamer", "Set up the mjpg-streamer server"
 
